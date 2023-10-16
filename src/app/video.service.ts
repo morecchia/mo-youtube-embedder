1edff235a1993d7ca97a3f2889e71f361dfd4a96
Michael Orecchia <michael.orecchia@gmail.com>
2023-10-16 12:55:47 -0700

    fix youtube api domain

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -26,7 +26,7 @@ export class VideoService {
     }
 
     var tag = document.createElement('script');
-    tag.src = 'http://www.youtube.com/iframe_api';
+    tag.src = 'https://www.youtube.com/iframe_api';
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
