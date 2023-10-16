8ece48a9a1e79902fdc34b98c91c476903c68171
Michael Orecchia <michael.orecchia@gmail.com>
2023-10-16 16:12:38 -0700

    add random initial search term

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,17 +12,37 @@ import { SearchService } from '../search.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  term = new FormControl('stereolab');
+  term = new FormControl('');
 
   constructor(private search: SearchService) { }
   
   ngOnInit() {
-    this.getVideos();
+    this.getVideos(this.randomTerm());
   }
 
-  getVideos() {
+  getVideos(term = '') {
     this.search
-      .searchVideos(this.term.value || '')
+      .searchVideos(term || this.term.value)
       .subscribe(res => console.log(res));
+  }
+
+  private randomTerm(): string {
+    const termSelection = [
+      'stereolab',
+      'broadcast band',
+      'the gentle people',
+      'aphex twin',
+      'autechre',
+      'boards of canada',
+      'the orb',
+      'plaid idm',
+      'mouse on mars',
+      'lfo idm',
+      'seefeel',
+      'pan sonic',
+      'cabaret voltaire'
+    ];
+
+    return termSelection[Math.floor(Math.random() * termSelection.length)];
   }
 }
